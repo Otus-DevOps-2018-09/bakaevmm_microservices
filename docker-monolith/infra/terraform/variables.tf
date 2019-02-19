8ee33825be44bec8989ea527259b8e9351dfae60
bakaev <bakaev@MacBook-Pro-bakaev.local>
2019-02-19 15:22:56 +0300

    add hw *

---
+++ docker-monolith/infra/terraform/variables.tf
@@ -0,0 +1,35 @@
+variable project {
+  description = "Project ID"
+}
+
+variable region {
+  description = "Region"
+  default     = "europe-west1"
+}
+
+variable zone {
+  description = "Zone"
+  default     = "europe-west1-b"
+}
+
+variable public_key_path {
+  description = "Path to the public key used for ssh access"
+}
+
+variable private_key_path {
+  description = "Path to the private key used for ssh access"
+}
+
+variable disk_image {
+  description = "Disk image"
+}
+
+variable count {
+  description = "count VM"
+  default     = 1
+}
+
+variable source_ranges {
+  description = "Allowed IP addresses"
+  default     = ["0.0.0.0/0"]
+}
